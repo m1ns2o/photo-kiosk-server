e5d2d3407242a3174da099c69f0deffafbb27990
정민서 <srjms0128@gmail.com>
2023-10-30 02:21:53 +0900

    database auto clear cron, utc +9 change

--- external_server/main.py
+++ external_server/main.py
@@ -3,13 +3,15 @@ from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from sqlalchemy.orm import sessionmaker
 import base64
-import datetime
+from datetime import datetime, timedelta
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import os
 import uuid
-from models import Img, engine  # model.py에서 필요한 부분을 가져옵니다.
+from models import Img, engine
 import json
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import timezone
 
 SessionLocal = sessionmaker(bind=engine)
 
@@ -22,6 +24,32 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def db_clear():
+    db_session = SessionLocal()
+    try:
+        three_days_ago = datetime.now(timezone('Asia/Seoul')) - timedelta(days=3)
+        image_records = db_session.query(Img).filter(Img.date <= three_days_ago).all()
+        for record in image_records:
+            file_path = IMAGE_DIR + record.img
+            img_file = file_path+".jpg"
+            mp4_file = file_path+".mp4"
+            if os.path.exists(img_file):
+                os.remove(img_file)
+            if os.path.exists(mp4_file):
+                os.remove(mp4_file)
+            db_session.delete(record)
+        # image_records.delete(synchronize_session='fetch')
+        db_session.commit()
+    finally:
+        db_session.close()
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(db_clear, trigger="interval", hours=1)  # 30초마다 실행
+# scheduler.add_job(job_function, trigger="cron", hour=17, minute=30)  # 매일 오후 5시 30분에 실행
+
+scheduler.start()
 
 class ImageData(BaseModel):
     image_addr: str #이미지 qr코드 주소
@@ -56,6 +84,7 @@ async def run_on_startup():
     IMAGE_DIR = data["dir"]
     if not os.path.exists(IMAGE_DIR):
         os.makedirs(IMAGE_DIR)
+    db_clear()
 
 
 @app.post("/save")
@@ -66,7 +95,8 @@ async def save_image_async(data: ImageData):
     # 고유한 .jpg 파일 이름 생성
     file_uuid = str(uuid.uuid4())
     unique_filename = f"{file_uuid}.jpg"
-    file_path = os.path.join(IMAGE_DIR, unique_filename).replace("\\", "/")
+    # file_path = os.path.join(IMAGE_DIR[:-1], unique_filename).replace("\\", "/")
+    file_path = IMAGE_DIR + unique_filename
     print(unique_filename)
     print(file_path)
     # 이미지를 로컬 디렉토리에 .jpg 형식으로 저장
@@ -75,7 +105,7 @@ async def save_image_async(data: ImageData):
 
     # 이미지 파일 경로를 DB에 저장
     db_session = SessionLocal()
-    new_image = Img(img=file_uuid, addr=data.image_addr, date=datetime.datetime.utcnow())
+    new_image = Img(img=file_uuid, addr=data.image_addr, date=datetime.now(timezone('Asia/Seoul')))
     db_session.add(new_image)
     db_session.commit()
     db_session.refresh(new_image)
@@ -99,7 +129,7 @@ async def save_image_async(data: ImageData):
 
 @app.get("/download/{file_name}")
 async def download_file(file_name: str):
-    file_path = IMAGE_DIR+"/" + file_name  # 실제 파일 경로 지정
+    file_path = IMAGE_DIR + file_name  # 실제 파일 경로 지정
     return FileResponse(file_path, headers={"Content-Disposition": f"attachment; filename={file_name}"})
 
 
@@ -107,7 +137,8 @@ async def download_file(file_name: str):
 def receive_file(file: UploadFile):
     print('call')
     try:
-        file_path = os.path.join(IMAGE_DIR, file.filename).replace("\\", "/")
+        # file_path = os.path.join(IMAGE_DIR[:-1], file.filename).replace("\\", "/")
+        file_path = IMAGE_DIR + file.filename
         with open(file_path + ".mp4", "wb+") as buffer:
             buffer.write(file.file.read())
         return {"detail": "File received successfully."}
